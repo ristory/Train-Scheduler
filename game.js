66a020667b089ad6dee3974835ad1d8c17b6d600
hoang <hoangcao@mail.weber.edu>
2018-10-04 21:25:23 -0600

    add more function

--- game.js
+++ game.js
@@ -1,5 +1,34 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDS6aEn66tLvN2EYYIphJz1BDU4jChwn6U",
+    authDomain: "train-scheduler-c51bb.firebaseapp.com",
+    databaseURL: "https://train-scheduler-c51bb.firebaseio.com",
+    projectId: "train-scheduler-c51bb",
+    storageBucket: "train-scheduler-c51bb.appspot.com",
+    messagingSenderId: "1001015335893"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
+  var name = $("#train-name").val();
+  var des = $("#destination").val();    
+  var time = $("#time-start").val();
+  var fre = $("#frequency").val();
+
+
 $(".btn").on("click", function(event) {
     event.preventDefault();
+
+    database.ref().set({
+        Trainnameval: name,
+        Destinationval: des,
+        Frequencyval: fre,
+        Timeval: time
+      });
+
+      database.ref().on("value", function(value) {
+            
+
+      });
     var table = $("<tr>");
     var trainame = $("<td>" + "John " + "</td>");
     var destination = $("<td>" + "John"  + "</td>");
@@ -14,3 +43,4 @@ $(".btn").on("click", function(event) {
     $("tbody").append(table);
 
 })
+
