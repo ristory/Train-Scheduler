a94864072be5f9aadefb2502e6371c444b0d503c
hoang <hoangcao@mail.weber.edu>
2018-10-05 21:08:29 -0600

    finish project

--- game.js
+++ game.js
@@ -1,46 +1,62 @@
-  // Initialize Firebase
-  var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyDS6aEn66tLvN2EYYIphJz1BDU4jChwn6U",
     authDomain: "train-scheduler-c51bb.firebaseapp.com",
     databaseURL: "https://train-scheduler-c51bb.firebaseio.com",
     projectId: "train-scheduler-c51bb",
     storageBucket: "train-scheduler-c51bb.appspot.com",
     messagingSenderId: "1001015335893"
-  };
-  firebase.initializeApp(config);
-  var database = firebase.database();
-  var name = $("#train-name").val();
-  var des = $("#destination").val();    
-  var time = $("#time-start").val();
-  var fre = $("#frequency").val();
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+var name = "";
+var des = "";
+var time = "";
+var fre = "";
 
 
-$(".btn").on("click", function(event) {
+$(".btn").on("click", function (event) {
     event.preventDefault();
-
-    database.ref().set({
+    name = $("#train-name").val();
+    des = $("#destination").val();
+    time = $("#time-start").val();
+    fre = $("#frequency").val();
+    var newTrain = {
         Trainnameval: name,
         Destinationval: des,
         Frequencyval: fre,
-        Timeval: time
-      });
+        Timeval: time,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    };
+    database.ref().push(newTrain);
+    alert("New Train successfully added");
+    $("#train-name").val("");
+    $("#destination").val("");
+    $("#time-start").val("");
+    $("#frequency").val("");
+})
+database.ref().on("child_added", function (value) {
+    var tFrequency = value.val().Frequencyval;
+    var firstTime = value.val().Timeval;
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+    var currentTime = moment(value.val().dateAdded).format("hh:mm");
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    var tRemainder = diffTime % tFrequency;
+    var tMinutesTillTrain = tFrequency - tRemainder;
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
 
-      database.ref().on("value", function(value) {
-            
-
-      });
     var table = $("<tr>");
-    var trainame = $("<td>" + "John " + "</td>");
-    var destination = $("<td>" + "John"  + "</td>");
-    var frequency = $("<td>" + "John"  + "</td>");
-    var arrival = $("<td>" + "John " + "</td>");
-    var timeaway = $("<td>" + "John"  + "</td>");
+    var trainame = $("<td>" + value.val().Trainnameval + "</td>");
+    var destination = $("<td>" + value.val().Destinationval + "</td>");
+    var frequency = $("<td>" + tFrequency + "</td>");
+    var arrival = $("<td>" + moment(nextTrain).format("HH:mm") + "</td>");
+    var timeaway = $("<td>" + tMinutesTillTrain + "</td>");
     table.append(trainame);
     table.append(destination);
     table.append(frequency);
     table.append(arrival);
     table.append(timeaway);
     $("tbody").append(table);
-
-})
-
+}, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
